71999ea0ea8aa834d35bdfd0e1317f431715fdf6
Andrii <andrii.fanha@oa.edu.ua>
2025-11-24 21:30:39 +0200

    first commit

---
+++ vue-lab/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterLink to="/contact">Contact</RouterLink> </nav>
+  <router-view></router-view>
+  
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
